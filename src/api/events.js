090738fb608e9fff85b1654a1b5421a1da493334
Ethan Wolz <ethan@creadiv.io>
2019-10-09 14:30:46 -0600

    Update Events

--- src/api/events.js
+++ src/api/events.js
@@ -1,8 +1,24 @@
 const eventRouter = require('express').Router()
 
+const WATCHABLE_EVENTS = ['pull_request']
+
 eventRouter.post('/', (req, res) => {
-  console.log(req.body)
-  console.log('HEYYYY')
+  const event = req.header('X-GitHub-Event')
+
+  if (!WATCHABLE_EVENTS.includes(event)) return res.json('event not supported')
+
+  const hasDesignLabel = req.body.labels.find(
+    label => label.name.toLowerCase() === 'designer review requested'
+  )
+
+  if (!hasDesignLabel) return res.json('Nothing to do... Label not added')
+
+  // If Designer requested we use the api to create a new check
+  // Check is in progress
+  // Send notitification to Slack
+  // Once Kevin (designer approves) - Change status to his selection
+  // Add PR comments if any
+
   res.status(200).json('ok')
 })
 

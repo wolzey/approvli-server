d129e0a5ab682d67c0382cb94ac4a0b01e0c0ce2
Ethan Wolz <ethan@creadiv.io>
2019-10-10 15:43:42 -0600

    Await update decision

--- src/api/checks.js
+++ src/api/checks.js
@@ -13,10 +13,10 @@ checkRouter.post('/:id', (req, res) => {
   const { approved, comment } = req.body
 
   // With the comment we need to create a new PR comment
-  Check.findOne({ _id: req.params.id }, (err, check) => {
+  Check.findOne({ _id: req.params.id }, async (err, check) => {
     if (err) throw err
     if (check) {
-      check.updateDecision(req.body)
+      await check.updateDecision(req.body)
     } else {
       return res.status(400).json('Check does not exist')
     }

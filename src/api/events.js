142ee8708e0912a28976a5e430d31ee13214660a
Ethan Wolz <ethan@creadiv.io>
2019-10-10 13:43:02 -0600

    remove console

--- src/api/events.js
+++ src/api/events.js
@@ -21,6 +21,7 @@ eventRouter.post('/', async (req, res) => {
 
   if (!hasDesignLabel) return res.json('Nothing to do... Label not added')
   // Needs to create check in DB, initialization will create this.
+
   Check.findOneAndUpdate(
     {
       'pull_request.id': pull_request.id,

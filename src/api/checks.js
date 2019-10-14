71af4f3bd811eed763a6930de010969920ca826b
Ethan Wolz <ethan@creadiv.io>
2019-10-14 12:32:14 -0600

    Cleanup

--- src/api/checks.js
+++ src/api/checks.js
@@ -10,9 +10,6 @@ checkRouter.get('/:id', (req, res) => {
 })
 
 checkRouter.post('/:id', (req, res) => {
-  const { approved, comment } = req.body
-
-  // With the comment we need to create a new PR comment
   Check.findOne({ _id: req.params.id }, async (err, check) => {
     if (err) throw err
     if (check) {

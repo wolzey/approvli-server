fe1d7035950901c6ec9e05ddc4df7388b3204ee9
Ethan Wolz <ethan@creadiv.io>
2019-10-14 12:12:20 -0600

    test

--- src/models/Check.js
+++ src/models/Check.js
@@ -109,7 +109,7 @@ CheckSchema.methods.sendSlackNotification = async function() {
               username: 'Approvli',
               channel: `@${username}`,
               text: `
-          Hello!\n\n You have a new review request on ${this.user.login}'s PR.\n
+          Hello!\n\n You have a new review request on ${this.user.login}'s PR.
           Please access it here https://approvli.netlify.com/reviews/${this._id}`,
             },
           })

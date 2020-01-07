c9ae99332dfadf14c8e0adfe578dff168710f60d
Ethan Wolz <ethan@creadiv.io>
2020-01-07 13:09:56 -0700

    Use new sendgrid

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,29 +1,16 @@
-const nodemailer = require("nodemailer");
-const sgTransport = require("nodemailer-sendgrid-transport");
+const sgMail = require("@sendgrid/mail");
 
-const options = {
-  auth: {
-    api_user: process.env.SENDGRID_USERNAME,
-    api_key: process.env.SENDGRID_PASSWORD
-  }
-};
-
-const client = nodemailer.createTransport(sgTransport(options));
+sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 module.exports = {
-  sendDesignMessage(email, { id, name }) {
-    return new Promise((resolve, reject) => {
-      const template = {
-        from: "approver@approvli.com",
-        to: email,
-        subject: "You have a new Design Approval Request!",
-        html: `<h2>Hello!</h2><p>You have a new review request on ${name}'s PR.</p><p>Please access it at</p> <a href="https://approvli.netlify.com/reviews/${id}">https://approvli.netlify.com</a>`
-      };
+  async sendDesignMessage(email, { id, name }) {
+    const template = {
+      from: "approver@approvli.com",
+      to: email,
+      subject: "You have a new Design Approval Request!",
+      html: `<h2>Hello!</h2><p>You have a new review request on ${name}'s PR.</p><p>Please access it at</p> <a href="https://approvli.netlify.com/reviews/${id}">https://approvli.netlify.com</a>`
+    };
 
-      client.sendMail(template, function(err, info) {
-        if (err) return reject(err);
-        resolve(info.response);
-      });
-    });
+    return sgMail.send(template);
   }
 };
